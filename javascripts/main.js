c5454a6d63276233844c75ef4b75b09db714a756
Shane D Wilson <me@shanedwilson.com>
2018-10-15 21:28:03 -0500

    getting fish

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,13 @@
 // Filter fish that are "on sale"
 
 // Add fish to "Basket"
+
+
+// Load Fish
+$.get('../db/fishes.json')
+    .done((data) => {
+        console.log(data);
+})
+.fail((error) => {
+    console.log(error);
+})    
