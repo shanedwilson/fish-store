db0da3d8b49e49b5511cb3e0a19ef18303f6c346
Shane D Wilson <me@shanedwilson.com>
2018-10-16 18:46:21 -0500

    wip;

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,12 +2,37 @@
 
 // Add fish to "Basket"
 
+const writeFishes = (arrayofFishes) => {
+    let domString = '';
+    arrayofFishes.forEach((fish )=> {
+        domString += `
+        <div class="${fish.onSale ? 'on-sale' : ''} fish card col-md-6 col-md-offset-3">
+                <div class="thumbnail">
+                    <img src="${fish.imageSoure}" 
+                    alt="" width="40%">
+                    <div class="caption">
+                        <h3 id="thumbnail-label">${fish.name}</h3>
+                        <p>$
+                            <span class="price">${fish.basePrice}</span>
+                        </p>
+                    </div>
+                    <div class="caption card-footer">
+                        <button class="add btn btn-danger">Add To Basket</button>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+    //write to dom
+    $("#available").append(domString);
+}
 
 // Load Fish
 $.get('../db/fishes.json')
     .done((data) => {
         console.log(data);
+        writeFishes(data.fishes);
 })
 .fail((error) => {
     console.log(error);
-})    
+}) 
